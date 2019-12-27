bc18847def43b2521f2e8845edd7e793d4eadd54
David Chia <david.mhchia@gmail.com>
2019-12-27 17:55:27 +0000

    Add google maps and logic to update map and marker when new postcode is keyed in

--- src/app.py
+++ src/app.py
@@ -1,14 +1,29 @@
+import os
 from urllib import parse
 
 from flask import Flask, render_template, request, jsonify
+from flask_googlemaps import GoogleMaps, Map
 import requests
 
 app = Flask(__name__)
-
+app.config['GOOGLEMAPS_KEY'] = os.environ.get('GOOGLE_API_KEY')
+GoogleMaps(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    default_latlng = (51.52349, -0.144435)  # latLng for W1W 5PN
+
+    google_map = Map(
+        identifier="google-map",
+        lat=default_latlng[0],
+        lng=default_latlng[1],
+        style="height:400px;width:800px;margin:0;",
+        maptype_control=False,
+        fullscreen_control=False,
+        streetview_control=False
+    )
+
+    return render_template("index.html", google_map=google_map)
 
 
 @app.route("/postcode", methods=["POST"])
