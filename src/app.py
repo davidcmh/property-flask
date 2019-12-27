5bf6456cfd3ca4b92af611827fe46a7201dfbc40
David Chia <david.mhchia@gmail.com>
2019-12-27 14:00:57 +0000

    Create layout.html as base template. Add form and endpoint to make API call to postcodes.io

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from urllib import parse
+
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +9,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/postcode", methods=["POST"])
+def postcode():
+    """Get postcode info from postcodes.io"""
+    input_postcode = request.form.get("postcode")
+    res = requests.get(f"https://postcodes.io/postcodes/{parse.quote(input_postcode)}")
+    return res.text
 
 
 if __name__ == "__main__":
